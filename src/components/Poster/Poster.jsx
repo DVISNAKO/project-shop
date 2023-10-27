18f39a562abf54517d66c2b68cab250eb3c9280c
Dima <dmitrijs.visnakovs@inbox.lv>
2023-10-27 08:18:39 +0300

    add modal

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 const Poster = () => {
+
+  const [modal, setModal] = useState(false);
+
+  const visible = modal ? '' : 'hidden';
+
     return (
         <section className={styles.home}>
         <div className={styles.title}>BIG SALE 20%</div>
@@ -9,7 +14,15 @@ const Poster = () => {
           <div className={styles.text}>
             <div className={styles.subtitle}>the bestseller of 2022</div>
             <h1 className={styles.head}>LENNON r2d2 with NVIDIA 5090 TI</h1>
-            <button className={styles.button}>Shop Now</button>
+            <button className={styles.button}
+            onClick={() => setModal(!modal)}>Shop Now</button>
+            <div className={`${visible} modal`}>
+            <div style={{display: 'flex',justifyContent: 'flex-end', cursor: 'pointer', fontSize: '20px'}}
+            onClick={() => setModal(!modal)}
+            >X</div>
+            <div style={{display: 'flex',justifyContent: 'center'}}>My modal</div>
+            </div>
+
           </div>
         </div>
       </section>
